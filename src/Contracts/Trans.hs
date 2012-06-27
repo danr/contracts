f5173bd94f187c72dcc3a209d01c9e4f2212a0a8
Dan Rosén <danr@student.chalmers.se>
2012-06-27 19:09:37 +0100

    Add a pass that makes min x -> x /= unr

--- src/Contracts/Trans.hs
+++ src/Contracts/Trans.hs
@@ -79,28 +79,38 @@ trPlain stm@(Statement n v c deps) = do
 trPos :: CoreExpr -> Contract -> HaloM Formula'
 trPos e c = case c of
     Pred p -> do
+        x    <- trExpr e
         p_tr <- trExpr p
-        return $ min' p_tr ==> (p_tr === con trueDataConId
-                             \/ p_tr === constant UNR)
+        return $ ors
+            [p_tr === con trueDataConId
+            ,p_tr === constant UNR
+            -- ,x    === constant UNR
+            -- do we need this?
+            ]
     CF -> do
         e_tr <- trExpr e
-        return $ min' e_tr ==> cf e_tr
+        return $ cf e_tr
     And c1 c2 -> (/\) <$> trPos e c1 <*> trPos e c2
     Arrow v c1 c2 -> local (pushQuant [v]) $ do
         fx <- trExpr (e `App` Var v)
         l <- trNeg (Var v) c1
         r <- trPos (e `App` Var v) c2
-        return $ forall' [v] (min' fx ==> (l \/ r))
+        return $ forall' [v] (l \/ r)
 
 trNeg :: CoreExpr -> Contract -> HaloM Formula'
 trNeg e c = case c of
     Pred p -> do
+        x <- trExpr e
         p_tr <- trExpr p
-        return $ min' p_tr /\ (p_tr === con falseDataConId
-                            \/ p_tr === constant BAD)
+        return $ ors
+            [p_tr === con falseDataConId
+            ,p_tr === constant BAD
+            -- ,x    === constant UNR
+            -- do we need this?
+            ]
     CF -> do
         e_tr <- trExpr e
-        return $ min' e_tr /\ neg (cf e_tr)
+        return $ neg (cf e_tr)
     And c1 c2 -> (\/) <$> trNeg e c1 <*> trNeg e c2
     Arrow v c1 c2 -> local (pushQuant [v]) $ do
         l <- trPos (Var v) c1
