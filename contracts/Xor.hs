1f93b42f538cd7851a52ff2c729ade4dd1451344
Dimitrios.Vytiniotis <dimitris@microsoft.com>
2012-07-06 13:44:30 +0100

    Strengthening the contract of Xor and ind.

--- contracts/Xor.hs
+++ contracts/Xor.hs
@@ -14,10 +14,9 @@ invariant f = case f of
     Xor x y -> invariant x && invariant y
     Lit     -> True
 
--- | Negating a formula
-neg :: Formula v -> Formula v
-neg (Xor f1 f2) = Xor (neg f1) (neg f2)
-neg Lit         = Lit
+ind :: Formula v -> Formula v
+ind (Xor f1 f2) = Xor (ind f1) (ind f2)
+ind Lit         = Lit
 
 (&&) :: Bool -> Bool -> Bool
 True  && b = b
@@ -28,30 +27,18 @@ and_cf = (&&) ::: CF --> CF --> CF
 -- | Invariant is crash free
 invariant_cf = invariant ::: CF --> CF
 
--- | Negating is crash free
-neg_contr_cf = neg ::: CF --> CF
+ind_contr_cf = ind ::: CF --> CF
 
--- | Negating retains the invariant /and/ is crash free
-neg_contr_retain
-    = neg ::: (CF --> CF)
-          :&: ((CF :&: Pred invariant) :-> \ y -> (CF :&: Pred invariant))
+-- | Indating retains the invariant /and/ is crash free
+ind_contr_retain
+    = ind -- ::: (CF --> CF)
+          ::: (Pred invariant :-> 
+                       \ y -> (Pred (\r -> invariant y && invariant r)))
+          -- :&: (CF :&: Pred invariant --> Pred invariant)
     `Using`
       invariant_cf
     `Using`
       and_cf
+      
+               
 
-{-
-
-   Wow, this is not true:
-
-     y = Xor UNR x
-     x = Xor Lit x
-
-     invariant Lit = True
-     invariant UNR = UNR
-     invariant y = UNR (= invariant UNR && invariant x = UNR && False = UNR)
-     invariant x = False (= invariant Lit && invariant x = invariant x)
-
-   but this is non-sensical, invariant x should be UNR.
-
--}
