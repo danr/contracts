3fc4951fdceaf3adad6ebeef34e586074412fd77
Dan Rosén <danr@student.chalmers.se>
2012-07-10 20:37:01 +0100

    Add two more tests

---
+++ tests/Foldr1.hs
@@ -0,0 +1,26 @@
+module Foldr1 where
+
+import Contracts
+import Prelude(Bool(..),error)
+
+null [] = True
+null xs = False
+
+not True = False
+not False = True
+
+f . g = \x -> f (g x)
+
+foldr1           :: (a -> a -> a) -> [a] -> a
+foldr1 f [x]     =  x
+foldr1 f (x:xs)  =  f x (foldr1 f xs)
+foldr1 _ []      =  error "Prelude.foldr1: empty list"
+
+unsat_foldr1_cf = foldr1
+    ::: (CF --> CF --> CF) --> CF :&: Pred (not . null) --> CF
+
+sat_foldr1_cf_only_pred = foldr1
+    ::: (CF --> CF --> CF) --> Pred (not . null) --> CF
+
+sat_foldr1_cf_broken = foldr1
+    ::: (CF --> CF --> CF) --> CF --> CF
