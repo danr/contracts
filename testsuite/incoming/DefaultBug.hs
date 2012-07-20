0b098966c5d60915810e7b5d367af608a033716e
Dan Rosén <danr@student.chalmers.se>
2012-07-20 16:20:25 +0100

    Give example of the DEFAULT bug with (||) function

--- testsuite/incoming/DefaultBug.hs
+++ testsuite/incoming/DefaultBug.hs
@@ -12,3 +12,11 @@ mini False y    = Ok
 
 -- This used to be a bug, should still be one when optimisation is off
 mini_cf = mini ::: CF --> CF --> CF
+
+-- As or
+(||) :: Bool -> Bool -> Bool
+True  || y    = True
+x     || True = True
+False || y    = False
+
+or_cf = (||) ::: CF --> CF --> CF
