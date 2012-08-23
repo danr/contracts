0e61f86e603582730dce9304f73f0e4c694c6dff
Dan Rosén <danr@student.chalmers.se>
2012-08-23 13:17:19 +0100

    Try to make the Shrink example simpler without success

--- testsuite/Shrink.hs
+++ testsuite/Shrink.hs
@@ -1,29 +1,15 @@
 module Shrink where
 
 import Contracts
-import Prelude(Bool(..),error)
-
-data Maybe a = Just a | Nothing
+import Prelude(Bool(..),Maybe(..),error,(&&),not,(.),null)
+import Data.Maybe (fromJust)
+import List
 
 isJust :: Maybe a -> Bool
 isJust (Just x) = True
 isJust Nothing  = False
 
-fromJust :: Maybe a -> a
-fromJust (Just x) = x
-fromJust Nothing  = error "fromJust!"
-
 fromJust_contr = fromJust ::: CF :&: Pred isJust --> CF
-
-all :: (a -> Bool) -> [a] -> Bool
-all p (x:xs) = p x && all p xs
-all p []     = True
-
-True  && b = b
-False && _ = False
-
-nonEmpty [] = False
-nonEmpty (_:_) = True
 
 shrink :: (a -> a -> a) -> [Maybe a] -> a
 shrink op []          = error "Empty list!"
@@ -36,6 +22,11 @@ shrink_lazy op []     = error "Empty list!"
 shrink_lazy op [x]    = fromJust x
 shrink_lazy op (x:xs) = fromJust x `op` shrink_lazy op xs
 
+-- Less efficient to use (not . null) :(
+-- nonEmpty = not . null
+nonEmpty (_:_) = True
+nonEmpty []    = False
+
 shrink_cf = shrink
     ::: (CF --> CF --> CF) --> CF :&: Pred nonEmpty :&: Pred (all isJust) --> CF
 
@@ -43,3 +34,18 @@ broken_shrink_lazy = shrink_lazy
     ::: (CF --> CF --> CF) :->
         \ op -> CF :&: Pred nonEmpty :&: Pred (all isJust) :->
         \ xs -> CF
+
+simpler :: (a -> b) -> Maybe a -> b
+simpler op (Just x) = op x
+simpler op Nothing  = error "Nothing!"
+
+simpler_cf = simpler ::: (CF --> CF) --> CF :&: Pred isJust --> CF
+
+simpler_lazy :: (a -> b) -> Maybe a -> b
+simpler_lazy op m = op (fromJust m)
+
+simpler_lazy_cf = simpler_lazy ::: (CF --> CF) --> CF :&: Pred isJust --> CF
+
+combine op m1 m2 = fromJust m1 `op` fromJust m2
+
+combine_cf = combine ::: (CF --> CF --> CF) --> CF :&: Pred isJust --> CF :&: Pred isJust --> CF
