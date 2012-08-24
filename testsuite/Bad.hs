957b3057dd9624cd21a6ffaa5788a714277c9046
Dan Rosén <danr@student.chalmers.se>
2012-08-24 16:39:51 +0100

    Cannot actually use undefined directly in contracts yet

--- testsuite/Bad.hs
+++ testsuite/Bad.hs
@@ -9,20 +9,24 @@ w2 :: ()
 w2 = case True of
        False -> ()
 
-w3 :: ()
-w3 = undefined
-
 w_ef :: Statement ()
 w_ef = w ::: CF
 
 w2_ef :: Statement ()
 w2_ef = w2 ::: CF
 
-w3_ef :: Statement ()
-w3_ef = w2 ::: CF
 
 w_list_ef :: Statement [()]
 w_list_ef = [w] ::: CF
 
 w_list_ef_2 :: Statement [()]
 w_list_ef_2 = [(),(),(),w,(),()] ::: CF
+
+{- Broken for now
+w3 :: ()
+w3 = undefined
+
+w3_ef :: Statement ()
+w3_ef = w3 ::: CF
+-}
+
