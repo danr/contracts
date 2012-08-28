d499bf5558904689c69e9d5662d97f9b2908176e
Dan Rosén <danr@student.chalmers.se>
2012-08-28 16:35:53 +0100

    Update MapFromJust; cannot countersatisfy the helpers

--- testsuite/MapFromJust.hs
+++ testsuite/MapFromJust.hs
@@ -9,24 +9,32 @@ isJust :: Maybe a -> Bool
 isJust (Just x) = True
 isJust Nothing  = False
 
-map_pred_helper = All $ \p -> All $ \f ->
+map_pred_helper_broken = All $ \p -> All $ \f ->
+    p ::: CF --> CF :=>
     f ::: CF :&: Pred p --> CF :=>
     map f ::: CF :&: Pred (all p) --> CF
 
-map_pred_helper_alt = All $ \p ->
+map_pred_helper_help_broken = (All $ \p -> All $ \f ->
+    p ::: CF --> CF :=>
+    f ::: CF :&: Pred p --> CF :=>
+    map f ::: CF :&: Pred (all p) --> CF)
+  `Using` isJust ::: CF --> CF
+  `Using` fromJust ::: CF :&: Pred isJust --> CF
+
+map_pred_helper_alt_broken = All $ \p ->
+    p ::: CF --> CF :=>
     map ::: (CF :&: Pred p --> CF) --> CF :&: Pred (all p) --> CF
 
-map_fromJust_cf = map fromJust ::: CF :&: Pred (all isJust) --> CF
-  `Using` map_pred_helper
+map_pred_helper_alt_help_broken = (All $ \p ->
+    p ::: CF --> CF :=>
+    map ::: (CF :&: Pred p --> CF) --> CF :&: Pred (all p) --> CF)
+  `Using` isJust ::: CF --> CF
+  `Using` fromJust ::: CF :&: Pred isJust --> CF
 
-map_fromJust_cf_alt = map fromJust ::: CF :&: Pred (all isJust) --> CF
-  `Using` map_pred_helper_alt
-
--- Proving it directly doesn't work
 map_fromJust_cf_broken = map fromJust ::: CF :&: Pred (all isJust) --> CF
 
--- Even if you assume some CF for all and isJust
 map_fromJust_cf_all_broken = map fromJust ::: CF :&: Pred (all isJust) --> CF
   `Using` all_cf
   `Using` (isJust ::: CF :&: Pred isJust --> CF)
+
 
