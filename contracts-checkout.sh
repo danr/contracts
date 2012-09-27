fea36a2c8f4d777cf6c4ca5683c3a2c4218cc632
Dimitrios.Vytiniotis <dimitris@microsoft.com>
2012-09-27 11:29:53 +0100

    Adding checkout script to avoid working with detached HEAD.

---
+++ contracts-checkout.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+BRANCH=$1
+
+if [ $# -ne 1 ]; then
+    echo "Usage: contracts-checkout <branch-name>"
+else
+    echo "Checking out contracts branch $BRANCH"
+    git checkout $BRANCH
+    git submodule update
+    HC=`cd halo && git log --pretty=oneline -1 HEAD`
+    BC=`cd halo && git log --pretty=oneline -1 $BRANCH`
+    if [ "$HC" != "$BC" ]; then
+    	echo "Aborting: halo submodule not in sync with $BRANCH"
+    else
+    	cd halo && git checkout $BRANCH
+    fi
+fi
